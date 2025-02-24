b09500be902d8c67721047cb6f19eb987be8a986
Xan-col <74800487+Xan-col@users.noreply.github.com>
2025-02-24 14:15:50 -0600

    ddddddd

---
+++ TestAverager_CH9/TestAverager_CH9/DisplayResults.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+#include <iomanip>
+
+using namespace std;
+
+void displayData(double array[], int size, double average)
+{
+	cout << "Sorted Grades\n";
+	cout << "-------------\n";
+
+	unique_ptr<int> index(new int);
+
+	for (*index = 0; *index < size; *index += 1)
+	{
+		cout << fixed << setprecision(2) << array[*index] << "\n";
+	}
+
+	cout << "\n";
+	cout << "Average\n";
+	cout << "-------------\n";
+	cout << fixed << setprecision(2) << average << "\n";
+
+	return;
+}
